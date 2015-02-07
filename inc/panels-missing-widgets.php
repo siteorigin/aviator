8a60266e1d6a808091930cfcc2c7c7266cb42aba
gregpriday <greg@siteorigin.com>
2015-02-07 23:28:14 +0200

    Added carousel fallback widget.

--- inc/panels-missing-widgets.php
+++ inc/panels-missing-widgets.php
@@ -21,12 +21,26 @@ function aviator_panels_missing_widgets($code, $widget, $args, $instance){
 			echo $args['after_widget'];
 			$code = ob_get_clean();
 			break;
+
+		case 'SiteOrigin_Widget_PostCarousel_Widget':
+			ob_start();
+			echo $args['before_widget'];
+			aviator_panels_missing_widgets_carousel($instance, $args);
+			echo $args['after_widget'];
+			$code = ob_get_clean();
+			break;
 	}
 
 	return $code;
 }
 add_filter('siteorigin_panels_missing_widget', 'aviator_panels_missing_widgets', 10, 4);
 
+/**
+ * Render the slider widget.
+ *
+ * @param $instance
+ * @param $args
+ */
 function aviator_panels_missing_widgets_slider($instance, $args){
 	$js_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
@@ -39,13 +53,30 @@ function aviator_panels_missing_widgets_slider($instance, $args){
 	get_template_part('demo/slider/slider');
 }
 
+/**
+ * Render the features widget
+ *
+ * @param $instance
+ * @param $args
+ */
 function aviator_panels_missing_widgets_features($instance, $args){
 	global $aviator_demo_features_instance;
-
 	// enqueue all the required styles
 	wp_enqueue_style('aviator-demo-features', get_template_directory_uri() . '/demo/features/css/style.css', array(), SITEORIGIN_THEME_VERSION);
 	wp_enqueue_style('aviator-demo-features-fontawesome', get_template_directory_uri() . '/demo/features/css/fontawesome/style.css', array(), SITEORIGIN_THEME_VERSION);
 
 	$aviator_demo_features_instance = $instance;
 	get_template_part('demo/features/features');
+}
+
+/**
+ * Render the carousel widget
+ */
+function aviator_panels_missing_widgets_carousel($instance, $args) {
+	$js_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+	wp_enqueue_style('aviator-demo-carousel-basic', get_template_directory_uri() . '/demo/carousel/css/style.css', array(), SITEORIGIN_THEME_VERSION);
+	wp_enqueue_script('aviator-demo-carousel-basic', get_template_directory_uri() . '/demo/carousel/js/carousel' . $js_suffix . '.js', array('jquery'), SITEORIGIN_THEME_VERSION);
+
+	get_template_part('demo/carousel/carousel');
 }
