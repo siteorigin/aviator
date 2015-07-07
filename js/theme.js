fa8b86b74ff6817c682b98e6b70b1a51dd3c3d91
Greg Priday <greg@siteorigin.com>
2015-07-07 15:17:19 +0200

    Fixed sticky with WordPress admin bar on mobile

--- js/theme.js
+++ js/theme.js
@@ -2,9 +2,16 @@ jQuery(function($){
     $('.entry-content').fitVids();
 
     if( aviator.navSticky ) {
-        $('#site-navigation').sticky({
-            topSpacing: ( $('#wpadminbar').length ? $('#wpadminbar').outerHeight() : 0 )
-        });
+        $('#site-navigation').sticky( {
+            topSpacing: ( $('#wpadminbar').css('position') !== 'absolute' && $('#wpadminbar').length ? $('#wpadminbar').outerHeight() : 0 )
+        } );
+
+        $(window).resize( function(){
+            // Reset the sticky when the window resizes to account for changing admin bar positioning.
+            $('#site-navigation').unstick().sticky( {
+                topSpacing: ( $('#wpadminbar').css('position') !== 'absolute' && $('#wpadminbar').length ? $('#wpadminbar').outerHeight() : 0 )
+            } );
+        } );
     }
 
 });
